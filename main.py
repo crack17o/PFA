70b6cfa6e708423dd9afb2499222c3433ef4ff46
Joël Stone <un1croyable17@gmail.com>
2025-07-22 11:39:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from app import create_app, db
 from app.routes import register_blueprints
 
+# Crée l'application
 app = create_app()
 register_blueprints(app)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    # Modification pour écouter sur 0.0.0.0 pour le déploiement sur Render
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
